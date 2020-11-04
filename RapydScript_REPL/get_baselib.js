c7e89283b7950d593b07b2312b9571e2c39674d1
Maradona <mardpharm86@gmail.com>
2020-11-04 21:08:41 +0700

    Update get_baselib.js

--- RapydScript_REPL/get_baselib.js
+++ RapydScript_REPL/get_baselib.js
@@ -13,10 +13,10 @@ var get_baselib = function (ՐՏ_baselib, ՐՏ_ver_url, onprogresscallback, onlo
                 if (localStorage && localStorage.getItem('ՐՏ_ver')) {
                     if (new_ver === localStorage.getItem('ՐՏ_ver')) {
                         if (localStorage.getItem('ՐՏ_baselib')) {
-                            get_script(null, onprogresscallback, onloadedcallback, null, 'ՐՏ_baselib');
+                            get_script(null, false, onprogresscallback, onloadedcallback, null, 'ՐՏ_baselib');
                         } else {
                             localStorage.removeItem('ՐՏ_ver');
-                            get_script(ՐՏ_baselib, onprogresscallback, function () {
+                            get_script(ՐՏ_baselib, false, onprogresscallback, function () {
                                 localStorage.setItem('ՐՏ_ver', new_ver);
                                 onloadedcallback();
                             }, onfailedcallback, 'ՐՏ_baselib');
@@ -26,29 +26,29 @@ var get_baselib = function (ՐՏ_baselib, ՐՏ_ver_url, onprogresscallback, onlo
                         oldscript_ver = localStorage.getItem('ՐՏ_ver');
                         localStorage.removeItem('ՐՏ_baselib');
                         localStorage.removeItem('ՐՏ_ver');
-                        get_script(ՐՏ_baselib, onprogresscallback, function () {
+                        get_script(ՐՏ_baselib, false, onprogresscallback, function () {
                             localStorage.setItem('ՐՏ_ver', new_ver);
                             onloadedcallback();
                         }, function (e) {
                             localStorage.setItem('ՐՏ_baselib', oldscript);
                             localStorage.setItem('ՐՏ_ver', oldscript_ver);
-                            get_script(null, onprogresscallback, onloadedcallback, null, 'ՐՏ_baselib');
+                            get_script(null, false, onprogresscallback, onloadedcallback, null, 'ՐՏ_baselib');
                         }, 'ՐՏ_baselib');
                     } else {
                         localStorage.removeItem('ՐՏ_ver');
-                        get_script(ՐՏ_baselib, onprogresscallback, function () {
+                        get_script(ՐՏ_baselib, false, onprogresscallback, function () {
                             localStorage.setItem('ՐՏ_ver', new_ver);
                             onloadedcallback();
                         }, onfailedcallback, 'ՐՏ_baselib');
                     }
                 } else if (localStorage) {
-                    get_script(ՐՏ_baselib, onprogresscallback, function () {
+                    get_script(ՐՏ_baselib, false, onprogresscallback, function () {
                         localStorage.setItem('ՐՏ_ver', new_ver);
                         onloadedcallback();
                     }, onfailedcallback, 'ՐՏ_baselib');
                 }
             } else {
-                get_script(null, onprogresscallback, onloadedcallback, onfailedcallback, 'ՐՏ_baselib');
+                get_script(null, false, onprogresscallback, onloadedcallback, onfailedcallback, 'ՐՏ_baselib');
             }
         }
     }
